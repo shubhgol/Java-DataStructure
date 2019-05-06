972ac552935c21d4960d0d843e46f6311cf64482
shubhkm <shubham.madheshiya@successive.tech>
2019-05-06 17:40:49 +0530

    singly linked list in java

--- LinkedList/linkList.java
+++ LinkedList/linkList.java
@@ -1,64 +1,45 @@
-import java.util.*;
 
-class LinkedList {
-    Node head=null;
-
-    static class Node {
-     private   int data;
-     private   Node next;
-        Node(int data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
-
-    public static  LinkedList insert(LinkedList list ,int data) {
-        Node newNode = new Node(data);
-        if(list.head== null ) {
-            list.head = newNode;
-        }
-        else {
-            Node currentNode = list.head;
-            while(currentNode.next!=null) 
-            currentNode = currentNode.next;
-
-            currentNode.next = newNode;
-        }
-        return list;
-    }
-
-    public static void printList(LinkedList list) {
-        Node currentNode = list.head;
-        while(currentNode!=null) {
-            System.out.print(currentNode.data+" ");
-            currentNode = currentNode.next;
-        }
-    }
-
-
-    public static void main(String[] args) 
-    { 
-        /* Start with the empty list. */
-        LinkedList list = new LinkedList(); 
-  
-        // 
-        // ******INSERTION****** 
-        // 
-  
-        // Insert the values 
-        list = insert(list, 1); 
-        list = insert(list, 2); 
-        list = insert(list, 3); 
-        list = insert(list, 4); 
-        list = insert(list, 5); 
-        list = insert(list, 6); 
-        list = insert(list, 7); 
-        list = insert(list, 8); 
-  
-        // Print the LinkedList 
-        printList(list); 
+class Node {
+    int data;
+    Node next;
+    Node(int data) {
+        this.data = data;
+        this.next = null;
     } 
-
-
-
 }
+
+ class LinkedList {
+     Node head = null;
+     
+  public void insertData(LinkedList list, int value) {
+
+    Node newNode = new Node(value);
+    if(list.head==null) {
+        list.head = newNode;
+    } else {
+        Node currentNode = list.head;
+        while(currentNode.next!=null) 
+        currentNode = currentNode.next;
+        currentNode.next = newNode;
+    }
+  
+  }
+
+
+  public void printList(LinkedList list) {
+      Node currentNode = list.head;
+      while(currentNode!=null) {
+          System.out.print(currentNode.data+" ");
+          currentNode = currentNode.next;
+      }
+  }
+  
+  public static void main(String[] args) {
+      LinkedList list = new LinkedList();
+      list.insertData(list,5);
+      list.insertData(list,10);
+      list.insertData(list,15);
+      list.printList(list);
+  }
+
+ }
